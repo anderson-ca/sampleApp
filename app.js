f668a4f7faa2f079cee9f92c06a9a49387e3e932
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-06-28 13:38:49 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+//////////////////////////////////////
+// -> inport libraries (dependencies)
+//////////////////////////////////////
+const express = require('express'); // -> import the express library
+const exphbs = require('express-handlebars');
+/////////////////////////////////////
+
+
+const app = express(); // -> use the initializer express function and assign it to app variable
+
+const port = process.env.PORT || 5000; // -> declare port
+
+/////////////////////////
+// -> express middleware
+/////////////////////////
+app.use((req, res, next) => { // -> time stamp middleware
+  console.log('Time stamp: ' + Date.now());
+  next();
+});
+///////////////////////////
+// -> handlebars middleware
+///////////////////////////
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars')
+////////////////////
+// -> create routes
+///////////////////
+app.get('/', (req, res) => { // -> index page route
+  res.render('index', {
+    title: 'Index Page'
+  })
+});
+////////////////////////////////////////////////////////////
+// -> tell the app to listen to specified port on localhost
+////////////////////////////////////////////////////////////
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
