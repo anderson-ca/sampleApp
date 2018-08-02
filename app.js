7f51c68f8c2e6d3e75dd84462da2fb6585eacd83
andersoncardoso <anderson.d.cardoso@hotmail.com>
2018-08-02 15:57:50 -0400

    fix user serialization

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(function (req, res, next) { // -> set up global variables
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
-  res.locals.user = req.user;
+  res.locals.user = req.user || null;
   next();
 });
 ///////////////////////////
